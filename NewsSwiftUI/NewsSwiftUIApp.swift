dada79cb0cb4807157b2ac97475698d5268043eb
RAFET <rafetcanakay@gmail.com>
2025-02-03 10:07:08 +0300

    Initial Commit

---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by Rafet Can AKAY on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
